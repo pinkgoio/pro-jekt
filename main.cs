818ffe889e29f1efc02378f0d620c120b40db79a
Arnold Gaye <161695695+pinkgoio@users.noreply.github.com>
2024-02-29 10:09:41 +0200

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,28 @@
+	if (t.IsArray)
+				return GetArrayTypeName (t.GetElementType ());
+
+			switch (Type.GetTypeCode (t)) {
+			case TypeCode.Object:
+				switch (t.Namespace) {
+				case "System":
+					switch (t.Name) {
+					case "Object":
+					case "ValueType":
+						return "NSObject";
+					case "Void":
+						return "void";
+					default:
+						return GetObjCName (t);
+					}
+				default:
+					return GetObjCName (t);
+				}
+          public bool IsSelectorTaken (ProcessedMemberBase member)
+		{
+			var typeRegistration = GetRegistrationForType (member.DeclaringType);
+			if (typeRegistration != null) {
+				foreach (var selector in member.Selectors) {
+					if (typeRegistration.ContainsKey (selector))
+						return true;
+				}
+			}
